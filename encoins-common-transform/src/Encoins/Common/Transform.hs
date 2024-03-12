eec1ffe5735f1cebeef74e8646ee98bcc97d7261
Vladislav <>
2024-03-12 16:58:48 -0300

    Add json helpers

--- encoins-common-transform/src/Encoins/Common/Transform.hs
+++ encoins-common-transform/src/Encoins/Common/Transform.hs
@@ -2,8 +2,23 @@
 
 module Encoins.Common.Transform where
 
-import           Data.Text (Text, pack)
+import           Data.Aeson           (encode, ToJSON)
+import           Data.ByteString      (ByteString)
+import           Data.ByteString.Lazy (toStrict)
+import           Data.Text            (Text, pack)
+import           Data.Text.Encoding   (decodeUtf8)
 
+
+-- Text conversion
 
 toText :: Show a => a -> Text
 toText = pack . show
+
+
+-- JSON conversion
+
+toJsonText :: ToJSON a => a -> Text
+toJsonText = decodeUtf8 . toJsonStrict
+
+toJsonStrict :: ToJSON a => a -> ByteString
+toJsonStrict = toStrict . encode
